86389bfdcb3d2d0953aeecab8e77312f33420e04
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-28 10:03:30 +0500

    debugging

--- routes/test.js
+++ routes/test.js
@@ -4,6 +4,8 @@ const authenticateToken = require('../utils/auth-token')
 
 testRouter.post('/solve-test', authenticateToken,  testController.SolveTest)
 testRouter.post('/create', authenticateToken,  testController.createTest)
+testRouter.post('/user-info/:userId', authenticateToken,  testController.getUserAllScores)
+
 
 
 module.exports = testRouter
