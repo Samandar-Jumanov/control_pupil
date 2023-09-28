d2378160abe33d758410d5badca57fd1734d9475
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-28 13:45:31 +0500

    added authPage

---
+++ utils/authPage.js
@@ -0,0 +1,15 @@
+const authPage = async (permissions)  =>{
+    return (request , response , next ) =>{
+        const {role} = request.body 
+        if(permissions.includes(role)){
+            next()
+        }else {
+            return response.status(401).json({
+                message :'You have no acces'
+            })
+        }
+    }
+}
+
+
+module.exports = {authPage}
