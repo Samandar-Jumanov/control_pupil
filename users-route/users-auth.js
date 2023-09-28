d4d8213eef7b765f95f77ecdc9191c1c7acb8666
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-28 16:24:08 +0500

    added middleware

--- users-route/users-auth.js
+++ users-route/users-auth.js
@@ -1,8 +1,9 @@
 const usersAuthController = require('../users-controller/users-auth')
+const validateEmail = require('../utils/checkEmail')
 const usersAuthRouter = require('express').Router()
 
-usersAuthRouter.post('/signup',   usersAuthController.SignUp)
-usersAuthRouter.post('/login',  usersAuthController.Login)
+usersAuthRouter.post('/signup',  validateEmail(),    usersAuthController.SignUp)
+usersAuthRouter.post('/login',   validateEmail(),   usersAuthController.Login)
 
 
 module.exports = usersAuthRouter
--- utils/checkEmail.js
+++ utils/checkEmail.js
@@ -1,15 +1,16 @@
 const { body, validationResult } = require('express-validator');
 
-    const validateEmail = [
-        body('email').isEmail().normalizeEmail(),
-        (req, res, next) => {
-          const errors = validationResult(req);
-          if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-          }
-          next();
-        }
-      ];
+const validateEmail = () => {
+  return [
+    body('email').isEmail().normalizeEmail(),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    }
+  ];
+};
 
-
-module.exports = validateEmail
+module.exports = validateEmail;
