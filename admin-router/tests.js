dd3ad3cfa6dfef7ed90387ec2825d626c8d37ecc
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-29 11:03:33 +0500

    updated routes

--- admin-router/tests.js
+++ admin-router/tests.js
@@ -4,7 +4,18 @@ const { authPage } = require('../utils/authPage')
 
 
 
-adminTestRouter.post('/create-test',  authPage('admin', 'teacher'),   adminTestController.createTest)
+adminTestRouter.post('/create-test',  authPage('admin', 'teacher'),   adminTestController.createTest) //creates 
+adminTestRouter.post('/get-all',  authPage('admin', 'teacher'),   adminTestController.getAllTests) //get all 
+adminTestRouter.post('/get/:testId',  authPage('admin', 'teacher'),   adminTestController.getTestById) //get by id 
+adminTestRouter.post('/update-test/:testId',  authPage('admin', 'teacher'),   adminTestController.updateTest) //update test
+adminTestRouter.post('/delete-test/:testId',  authPage('admin', 'teacher'),   adminTestController.deleteTest) //delete  test
+
+
+
+
+
+
+
 
 module.exports = {adminTestRouter}
 
