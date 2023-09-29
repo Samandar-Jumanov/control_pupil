14724cbe632989a00957926a90ab6cc3c2de53ed
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-29 12:01:06 +0500

    fixing bugs

--- admin-router/theme.js
+++ admin-router/theme.js
@@ -3,10 +3,10 @@ const themeRouter = require('express').Router()
 const { authPage } = require('../utils/authPage')
 
 themeRouter.post('/create-theme',  authPage('admin', 'teacher'),    themeController.createTheme) //create theme 
-themeRouter.get('/get-all',  authPage('admin', 'teacher'),    themeController.getAllThemes)  // get all themes
-themeRouter.get('/get-theme/:themeId',  authPage('admin', 'teacher'),    themeController.getThemeById) // get theme by id 
-themeRouter.put('/update-theme/:themeId/:adminId',  themeController.updateTheme) // update theme 
-themeRouter.delete('/delete-theme/:themeId/:adminId',  authPage('admin', 'teacher') ,     themeController.deleteTheme) //  delete them 
+themeRouter.get('/get-all',     themeController.getAllThemes)  // get all themes
+themeRouter.get('/get-theme/:themeId',    themeController.getThemeById) // get theme by id 
+themeRouter.put('/update-theme/:themeId',  themeController.updateTheme) // update theme 
+themeRouter.delete('/delete-theme/:themeId/:adminId',   themeController.deleteTheme) //  delete them 
 
 
 module.exports = {themeRouter}
