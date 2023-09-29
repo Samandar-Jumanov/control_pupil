457ff609aa030b72f720254627aa0dade9a46036
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-09-29 14:01:17 +0500

    checking test routes

--- admin-router/tests.js
+++ admin-router/tests.js
@@ -5,10 +5,10 @@ const { authPage } = require('../utils/authPage')
 
 
 adminTestRouter.post('/create-test',  authPage('admin', 'teacher'),   adminTestController.createTest) //creates 
-adminTestRouter.post('/get-all',  authPage('admin', 'teacher'),   adminTestController.getAllTests) //get all 
-adminTestRouter.post('/get/:testId',  authPage('admin', 'teacher'),   adminTestController.getTestById) //get by id 
-adminTestRouter.post('/update-test/:testId',  authPage('admin', 'teacher'),   adminTestController.updateTest) //update test
-adminTestRouter.post('/delete-test/:testId',  authPage('admin', 'teacher'),   adminTestController.deleteTest) //delete  test
+adminTestRouter.get('/get-all',  authPage('admin', 'teacher'),   adminTestController.getAllTests) //get all 
+adminTestRouter.get('/get/:testId',  authPage('admin', 'teacher'),   adminTestController.getTestById) //get by id 
+adminTestRouter.put('/update-test/:testId',  authPage('admin', 'teacher'),   adminTestController.updateTest) //update test
+adminTestRouter.delete('/delete-test/:testId',  authPage('admin', 'teacher'),   adminTestController.deleteTest) //delete  test
 
 
 
