378628e38f7c3af52c649f2ba73c50400a9d4212
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-10-15 13:27:33 +0500

    fixing bugs

--- admin-controller/user-score.js
+++ admin-controller/user-score.js
@@ -18,44 +18,44 @@ const getAllUserScores = async (request , response , next ) =>{
         next(error)
     }
 }
-
-
 const getSingleUserScores = async (request, response, next) => {
     const { userId } = request.params;
         
-        await redisClient.get(`score?userId=${userId}`, async (err, data) => {
-            if (err) {
-               console.log(err.stack);
-               console.log("Missing data");
-            }
-
-            if (data != null) {
-                console.log('Hit');
-                response.json({
-                    data: JSON.parse(data)
-                });
-                return;
-            } else {
-                const user = await Users.findByPk(userId, {
-                    include: {
-                        model: Scores,
-                        as: 'scores'
-                    }
-                });
-                if (!user) {
-                    return response.status(404).json({
-                        message: 'User not found'
-                    });
+    try {
+        const data = await redisClient.get(`score?userId=${userId}`);
+        
+        if (data != null) {
+            console.log('Hit');
+            response.json({
+                data: JSON.parse(data)
+            });
+            return;
+        } else {
+            const user = await Users.findByPk(userId, {
+                include: {
+                    model: Scores,
+                    as: 'scores'
                 }
-                
-                const userAllScore = await user.getScores();
-                await  redisClient.set(`score?userId=${userId}`,  JSON.stringify(userAllScore));
-                return  response.status(200).json({
-                    userAllScore: userAllScore
+            });
+            if (!user) {
+                return response.status(404).json({
+                    message: 'User not found'
                 });
             }
+            
+            const userAllScore = await user.getScores();
+            await redisClient.set(`score?userId=${userId}`, JSON.stringify(userAllScore));
+            
+            response.status(200).json({
+                userAllScore: userAllScore
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({
+            message: error
         });
-
+    }
 }
 
-module.exports = {getAllUserScores , getSingleUserScores , redisClient}
+module.exports = {getAllUserScores , getSingleUserScores }
