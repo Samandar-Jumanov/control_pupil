dcb37784901bfc377e04f45d27b8c4ca0b86eb95
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-10-15 13:31:43 +0500

    updated reddis

--- utils/redis.js
+++ utils/redis.js
@@ -7,8 +7,8 @@ const redisClient = redis.createClient({
     socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT
-    }
-
+    },
+    legacyMode : true,
 })
 
 module.exports = {redisClient}
